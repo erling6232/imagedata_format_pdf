c1abebaaac72bbcf0d162f56d38a1230e1e3b796
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2026-05-29 07:15:19 +0200

    Set __version__

--- src/imagedata_format_pdf/__init__.py
+++ src/imagedata_format_pdf/__init__.py
@@ -4,5 +4,11 @@ import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
+try:
+    from importlib.metadata import version
+    __version__ = version('imagedata_format_pdf')
+except Exception:
+    __version__ = None
+
 __author__ = 'Erling Andersen, Haukeland University Hospital, Bergen, Norway'
 __email__ = 'Erling.Andersen@Helse-Bergen.NO'
